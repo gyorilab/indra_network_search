a02f0651a041efa32209fa587899b2edf5c4493b
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-09 21:09:16 +0100

    Fix getting metadata
    The old version doesn't work in a packaged context because setup.cfg isn't available.

--- indra_network_search/__init__.py
+++ indra_network_search/__init__.py
@@ -1,9 +1,4 @@
-from configparser import ConfigParser
-from pathlib import Path
+from importlib.metadata import version
 
-fp = Path(__file__).absolute().parent.parent.joinpath("setup.cfg").as_posix()
-cfp = ConfigParser()
-cfp.read(fp)
-
-NAME = cfp.get("metadata", "description")
-VERSION = cfp.get("metadata", "version")
+NAME = "INDRA Network Search"
+VERSION = version("indra_network_search")
